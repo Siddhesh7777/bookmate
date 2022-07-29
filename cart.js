5c508ab104713690c384eac42b643f0e455bba83
Siddhesh Patil <97019793+Siddhesh7777@users.noreply.github.com>
2022-07-30 03:27:03 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,12 @@
+document.querySelector(".btn1").addEventListener("click",function(){
+
+    document.querySelector(".ysp").textContent="Your Total is "+"₹"+260;
+    document.querySelector(".ysp").style.backgroundColor = "purple";
+    document.querySelector(".ysp").style.color = "white";
+});
+document.querySelector(".btn2").addEventListener("click",function(){
+
+  document.querySelector('.product').textContent = ".";
+
+
+});
